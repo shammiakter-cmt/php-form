c7d82d736274f9d4a154a2bdb1d728f8572595d3
shammiakter-cmt <116479156+shammiakter-cmt@users.noreply.github.com>
2022-12-19 22:41:01 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .error {colour: #ff0000};
+        </style>
+</head>
+<body>
+    <?php
+    // define variabales and set empty values
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(empty($_POST["name"])){
+            $nameErr="Name is requried";
+        } else {
+            $name = test_input($_POST["name"]);
+             // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+        $nameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if(empty($_POST["email"])){
+        $emailErr="Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+
+
+    if(empty($_POST["webside"])){
+        $website="";
+    } else {
+        $website = test_input($_POST["webside"]);
+    }
+
+
+    if(empty($_POST["comment"])){
+        $commentErr="Comment is required" ;
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+
+    if(empty($_POST["gender"])){
+        $genderErr="Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+
+    }
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+    
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+   
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+
+</html>
